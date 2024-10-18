96523ea5b6bbe7c822da6346067dfdb5cf5fb054
jmoler2 <114830361+jmoler2@users.noreply.github.com>
2024-10-17 22:11:29 -0400

    implemented travelling concept I think

--- client/views/HomeView.vue
+++ client/views/HomeView.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import PostListComponent from "@/components/Post/PostListComponent.vue";
+import LocationForm from "@/components/Travel/LocationForm.vue";
 import { useUserStore } from "@/stores/user";
+import { useLocationStore } from "@/stores/location";
 import { storeToRefs } from "pinia";
 
 const { currentUsername, isLoggedIn } = storeToRefs(useUserStore());
+const { currentLocation } = storeToRefs(useLocationStore());
 </script>
 
 <template>
@@ -12,6 +15,12 @@ const { currentUsername, isLoggedIn } = storeToRefs(useUserStore());
     <section>
       <h1 v-if="isLoggedIn">Welcome {{ currentUsername }}!</h1>
       <h1 v-else>Please login!</h1>
+    </section>
+    <section>
+      <h1 v-if="isLoggedIn">
+        <LocationForm />
+        Your location: {{ currentLocation }}
+      </h1>
     </section>
     <PostListComponent />
   </main>
