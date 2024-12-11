c03610f30696eb263eee4c726b9df3d4f12ecbfe
John Moler <johnm2@mit.edu>
2024-12-11 04:16:34 -0500

    made some ui changes

--- client/views/HomeView.vue
+++ client/views/HomeView.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import PostListComponent from "@/components/Post/PostListComponent.vue";
-import LocationForm from "@/components/Travel/LocationForm.vue";
+import LocationSetForm from "@/components/Travel/LocationForm.vue";
+import LocationChangeForm from "@/components/Travel/changeLocationForm.vue";
 import { useUserStore } from "@/stores/user";
 import { useLocationStore } from "@/stores/location";
 import { storeToRefs } from "pinia";
@@ -17,10 +18,16 @@ const { currentLocation } = storeToRefs(useLocationStore());
       <h1 v-else>Please login!</h1>
     </section>
     <section>
-      <h1 v-if="isLoggedIn">
-        <LocationForm />
-        Your location: {{ currentLocation }}
-      </h1>
+      <div v-if="isLoggedIn">
+        <div v-if="currentLocation">
+          <h2>Your current location: {{ currentLocation }}</h2>
+          <LocationChangeForm />
+        </div>
+        <div v-else>
+          <h2>Please set your location:</h2>
+          <LocationSetForm />
+        </div>
+      </div>
     </section>
     <PostListComponent />
   </main>
@@ -29,5 +36,10 @@ const { currentLocation } = storeToRefs(useLocationStore());
 <style scoped>
 h1 {
   text-align: center;
+}
+
+h2 {
+  text-align: center;
+  margin-top: 1em;
 }
 </style>
